ecc9ff582578eb1ba034eb6af577a1de13944044
Green <green.chiu@langlive.com>
2019-09-23 11:50:00 +0800

    Initial Commit

---
+++ CollapsingProfileController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollapsingProfileController
+//
+//  Created by GreenChiu on 2019/9/23.
+//  Copyright © 2019 GreenChiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
